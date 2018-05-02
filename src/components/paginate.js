a14da95072ab6aa3f00a4d1782eaabba39abfbe4
Muneeb Rabaney <muneeb.rabaney@liquorice.co.za>
2018-05-02 23:27:29 +0200

    test commit from dev branch

--- src/components/paginate.js
+++ src/components/paginate.js
@@ -1,11 +1,14 @@
-// import React, { Component } from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import Navigation from './src/components/navigation'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-// class Paginate extends Component {
-//   render() {
-//     return <Navigation />
-//   }
-// }
+class Paginate extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Pagination Component</Text>
+      </View> 
+    )
+  }
+}
 
-// export default App
+export default Paginate
