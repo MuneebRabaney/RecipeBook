ec3a15a2ea523352ba4b2316b3c699bb8d3e7a62
Muneeb Rabaney <muneeb.rabaney@liquorice.co.za>
2018-05-06 23:10:23 +0200

    loading background color changed and loader icon size changed

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View,  ActivityIndicator } from 'react-native'
 
+
 class Loading extends Component {
 
   constructor(props) {
@@ -8,11 +9,23 @@ class Loading extends Component {
   }
 
   render() {
-    return this.props.isLoading ? (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ActivityIndicator />
+    return this.props.isLoading ? 
+      <View style={{ 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        backgroundColor:  '#000000',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        height: '100%',
+        width: '100%',
+      }}>
+        <ActivityIndicator size='large' color="#fff" />
       </View>
-    ) : null
+     : null
   }
 }
 
